5c0d20a2605cc72e539d80f96cb9feda4cf9f315
Samuel Parfait <samuelparfaitjr@gmail.com>
2022-03-07 23:40:14 +0000

    Setup Heroku database

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}...`);
 });
